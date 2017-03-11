18b58366d2ef69862e1f334a35ea4d3ab6289a64
Serghei Iakovlev <serghei@phalconphp.com>
2017-03-11 20:44:22 +0200

    Improved environment vars

--- builder/vars.mk
+++ builder/vars.mk
@@ -13,10 +13,10 @@
 # Authors: Serghei Iakovlev <serghei@phalconphp.com>
 #
 
-gen-vars: get-info
+gen-host-vars: get-info
 	$(shell echo "#!/usr/bin/env bash" > $(SCRIPTDIR)/.variables.sh)
 	$(shell echo "export PRODUCT=\"$(PRODUCT)\"" >> $(SCRIPTDIR)/.variables.sh)
-	$(shell echo "export PRODUCT_EXTRA=\"$(PRODUCT_EXT'RA)\"" >> $(SCRIPTDIR)/.variables.sh)
+	$(shell echo "export PRODUCT_EXTRA=\"$(PRODUCT_EXTRA)\"" >> $(SCRIPTDIR)/.variables.sh)
 	$(shell echo "export PHALCON_DIR=\"$(PHALCON_DIR)\"" >> $(SCRIPTDIR)/.variables.sh)
 	$(shell echo "export BUILDDIR=\"$(BUILDDIR)\"" >> $(SCRIPTDIR)/.variables.sh)
 	$(shell echo "export SCRIPTDIR=\"$(SCRIPTDIR)\"" >> $(SCRIPTDIR)/.variables.sh)
@@ -47,45 +47,50 @@ gen-vars: get-info
 	$(shell echo "export CHANGELOG_EMAIL=\"$(CHANGELOG_EMAIL)\"" >> $(SCRIPTDIR)/.variables.sh)
 	$(shell echo "export CHANGELOG_TEXT=\"$(CHANGELOG_TEXT)\"" >> $(SCRIPTDIR)/.variables.sh)
 
+gen-docker-vars:
+	$(shell echo "PRODUCT_EXTRA=$(PRODUCT_EXTRA)" > $(BUILDDIR)/env)
+	$(shell echo "PHP_VERSION=$(PHP_VERSION)" >> $(BUILDDIR)/env)
+	$(shell echo "REPO_VENDOR=$(REPO_VENDOR)" >> $(BUILDDIR)/env)
+
 get-info:
 	@echo ""
 	@echo ""
-	@echo "   Product ............................: $(PRODUCT)"
-	@echo "   Product extra ......................: $(PRODUCT_EXTRA)"
+	@echo "   Product ............................: $(OK_COLOR)$(PRODUCT)$(NO_COLOR)"
+	@echo "   Product extra ......................: $(OK_COLOR)$(PRODUCT_EXTRA)$(NO_COLOR)"
 	@echo ""
-	@echo "   Product path .......................: $(PHALCON_DIR)"
-	@echo "   Build path .........................: $(BUILDDIR)"
-	@echo "   Current path .......................: $(SCRIPTDIR)"
+	@echo "   Product path .......................: $(OK_COLOR)$(PHALCON_DIR)$(NO_COLOR)"
+	@echo "   Build path .........................: $(OK_COLOR)$(BUILDDIR)$(NO_COLOR)"
+	@echo "   Current path .......................: $(OK_COLOR)$(SCRIPTDIR)$(NO_COLOR)"
 	@echo ""
-	@echo "   Stable branch/tag ..................: $(STABLE_BRANCH)"
-	@echo "   Nightly branch/tag .................: $(NIGHTLY_BRANCH)"
-	@echo "   Clone branch/tag ...................: $(CLONE_BRANCH)"
+	@echo "   Stable branch/tag ..................: $(OK_COLOR)$(STABLE_BRANCH)$(NO_COLOR)"
+	@echo "   Nightly branch/tag .................: $(OK_COLOR)$(NIGHTLY_BRANCH)$(NO_COLOR)"
+	@echo "   Clone branch/tag ...................: $(OK_COLOR)$(CLONE_BRANCH)$(NO_COLOR)"
 	@echo ""
-	@echo "   Build release ......................: $(RELEASE)"
-	@echo "   Revision ...........................: $(REVISION)"
-	@echo "   Semantic version: ..................: $(VERSION)"
-	@echo "   Full version name ..................: $(VERSION_FULL)"
+	@echo "   Build release ......................: $(OK_COLOR)$(RELEASE)$(NO_COLOR)"
+	@echo "   Revision ...........................: $(OK_COLOR)$(REVISION)$(NO_COLOR)"
+	@echo "   Semantic version: ..................: $(OK_COLOR)$(VERSION)$(NO_COLOR)"
+	@echo "   Full version name ..................: $(OK_COLOR)$(VERSION_FULL)$(NO_COLOR)"
 	@echo ""
-	@echo "   Travis PHP version .................: $(TRAVIS_PHP_VERSION)"
-	@echo "   PHP major version ..................: $(PHP_MAJOR)"
-	@echo "   Overrided PHP version ..............: $(PHP_VERSION)"
-	@echo "   Zephir version .....................: $(ZEPHIR_VERSION)"
-	@echo "   Zend Engine backend ................: $(ZEND_BACKEND)"
+	@echo "   Travis PHP version .................: $(OK_COLOR)$(TRAVIS_PHP_VERSION)$(NO_COLOR)"
+	@echo "   PHP major version ..................: $(OK_COLOR)$(PHP_MAJOR)$(NO_COLOR)"
+	@echo "   Overrided PHP version ..............: $(OK_COLOR)$(PHP_VERSION)$(NO_COLOR)"
+	@echo "   Zephir version .....................: $(OK_COLOR)$(ZEPHIR_VERSION)$(NO_COLOR)"
+	@echo "   Zend Engine backend ................: $(OK_COLOR)$(ZEND_BACKEND)$(NO_COLOR)"
 	@echo ""
-	@echo "   Repo vendor ........................: $(REPO_VENDOR)"
-	@echo "   OS .................................: $(OS)"
-	@echo "   Build OS ...........................: $(BUILD_OS)"
-	@echo "   Distrib. version ...................: $(DIST)"
+	@echo "   Repo vendor ........................: $(OK_COLOR)$(REPO_VENDOR)$(NO_COLOR)"
+	@echo "   OS .................................: $(OK_COLOR)$(OS)$(NO_COLOR)"
+	@echo "   Build OS ...........................: $(OK_COLOR)$(BUILD_OS)$(NO_COLOR)"
+	@echo "   Distrib. version ...................: $(OK_COLOR)$(DIST)$(NO_COLOR)"
 	@echo ""
-	@echo "   Packagecloud repo ..................: $(PACKAGECLOUD_REPO)"
-	@echo "   Docker repo ........................: $(DOCKER_REPO)"
-	@echo "   Docker image .......................: $(DOCKER_IMAGE)"
-	@echo "   Docker suffix ......................: $(DOCKER_SUFFIX)"
-	@echo "   Fully qualified Docker image .......: $(DOCKER_TAG)"
-	@echo "   Package type .......................: $(PACKAGE)"
+	@echo "   Packagecloud repo ..................: $(OK_COLOR)$(PACKAGECLOUD_REPO)$(NO_COLOR)"
+	@echo "   Docker repo ........................: $(OK_COLOR)$(DOCKER_REPO)$(NO_COLOR)"
+	@echo "   Docker image .......................: $(OK_COLOR)$(DOCKER_IMAGE)$(NO_COLOR)"
+	@echo "   Docker suffix ......................: $(OK_COLOR)$(DOCKER_SUFFIX)$(NO_COLOR)"
+	@echo "   Fully qualified Docker image .......: $(OK_COLOR)$(DOCKER_TAG)$(NO_COLOR)"
+	@echo "   Package type .......................: $(OK_COLOR)$(PACKAGE)$(NO_COLOR)"
 	@echo ""
-	@echo "   Changelog name .....................: $(CHANGELOG_NAME)"
-	@echo "   Changelog email ....................: $(CHANGELOG_EMAIL)"
-	@echo "   Changelog text .....................: $(CHANGELOG_TEXT)"
+	@echo "   Changelog name .....................: $(OK_COLOR)$(CHANGELOG_NAME)$(NO_COLOR)"
+	@echo "   Changelog email ....................: $(OK_COLOR)$(CHANGELOG_EMAIL)$(NO_COLOR)"
+	@echo "   Changelog text .....................: $(OK_COLOR)$(CHANGELOG_TEXT)$(NO_COLOR)"
 	@echo ""
 	@echo ""
