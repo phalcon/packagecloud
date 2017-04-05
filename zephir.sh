87a13ee171d60768db49e94bb9c30784fc171a82
Serghei Iakovlev <serghei@phalconphp.com>
2017-04-05 22:56:11 +0300

    Added script to install parser

--- zephir.sh
+++ zephir.sh
@@ -5,13 +5,13 @@
 # Copyright (c) 2011-2017, Phalcon Team (https://www.phalconphp.com)
 #
 # This source file is subject to the New BSD License that is bundled
-# with this package in the file LICENSE.txt
+# with this package in the file LICENSE.txt.
 #
 # If you did not receive a copy of the license and are unable to
 # obtain it through the world-wide-web, please send an email
 # to license@phalconphp.com so we can send you a copy immediately.
 #
-# Authors: Serghei Iakovlev <serghei@phalconphp.com>
+# Authors: Phalcon Framework Team <team@phalconphp.com>
 #
 
 git clone -q --depth=1 https://github.com/phalcon/zephir.git /tmp/zephir
